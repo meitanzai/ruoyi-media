32c575920a5082e0cc6e114a40bfedb0296bcc8d
tanhuihuang <tanhuihuang@richinfo.cn>
2023-03-24 10:02:12 +0800

    【fix】修复2.6.x路径匹配问题，参考文章：https://www.jianshu.com/p/4500b61384f8，2.5.8版本用的路径匹配策略是 AntPathMatcher  而 Spring Boot 2.6.x版本以上的默认匹配策略是 path-pattern-matcher

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/properties/PermitAllUrlProperties.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/properties/PermitAllUrlProperties.java
@@ -30,6 +30,10 @@ public class PermitAllUrlProperties implements InitializingBean, ApplicationCont
 
     public String ASTERISK = "*";
 
+    // https://www.jianshu.com/p/4500b61384f8  2.5.8 以及 2.6.x以上 有区别，路径匹配策略方式不一样
+    // 2.5.8版本用的路径匹配策略是 AntPathMatcher  而 Spring Boot 2.6.x版本以上的默认匹配策略是 path-pattern-matcher
+    // 2.5.8版本 info.getPatternsCondition() 有值， info.getPathPatternsCondition()为空，2.6.x则是相反
+    // 升级版本2.6.x后，swagger路径有变也是这个原因 可参考这个文章解决 https://www.jb51.net/article/244317.htm
     @Override
     public void afterPropertiesSet() {
         RequestMappingHandlerMapping mapping = applicationContext.getBean(RequestMappingHandlerMapping.class);
@@ -40,13 +44,13 @@ public class PermitAllUrlProperties implements InitializingBean, ApplicationCont
 
             // 获取方法上边的注解 替代path variable 为 *
             Anonymous method = AnnotationUtils.findAnnotation(handlerMethod.getMethod(), Anonymous.class);
-            Optional.ofNullable(method).ifPresent(anonymous -> Objects.requireNonNull(info.getPatternsCondition().getPatterns())
-                    .forEach(url -> urls.add(RegExUtils.replaceAll(url, PATTERN, ASTERISK))));
+            Optional.ofNullable(method).ifPresent(anonymous -> Objects.requireNonNull(info.getPathPatternsCondition().getPatterns())
+                    .forEach(url -> urls.add(RegExUtils.replaceAll(url.getPatternString(), PATTERN, ASTERISK))));
 
             // 获取类上边的注解, 替代path variable 为 *
             Anonymous controller = AnnotationUtils.findAnnotation(handlerMethod.getBeanType(), Anonymous.class);
-            Optional.ofNullable(controller).ifPresent(anonymous -> Objects.requireNonNull(info.getPatternsCondition().getPatterns())
-                    .forEach(url -> urls.add(RegExUtils.replaceAll(url, PATTERN, ASTERISK))));
+            Optional.ofNullable(controller).ifPresent(anonymous -> Objects.requireNonNull(info.getPathPatternsCondition().getPatterns())
+                    .forEach(url -> urls.add(RegExUtils.replaceAll(url.getPatternString(), PATTERN, ASTERISK))));
         });
     }
 
